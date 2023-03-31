495b985f57b10a9a633abc10b3f5c3ca0da6b119
BarneyTheDinos <minhblsws00028@fpt.edu.vn>
2023-03-30 18:14:47 -0800

    second commit

--- about.php
+++ about.php
@@ -17,7 +17,7 @@
 <body>
     <!--The header section starts-->
     <?php
-    include("header.html");
+    include("includes/header.html");
     ?>
     <!--The header section starts-->
 
@@ -260,7 +260,7 @@
 
     <!--The footer section starts-->
     <?php
-    include("footer.html");
+    include("includes/footer.html");
     ?>
     <!--The footer section ends-->
 
