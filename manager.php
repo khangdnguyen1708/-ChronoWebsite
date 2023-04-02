e91760beeea9b4fdde3922d33b55b2a3b1cc0fd7
LeQuangHai204 <lequanghai.private@gmail.com>
2023-04-02 17:33:24 +0700

    modified:   manager.php 	modified:   manager.php

--- manager.php
+++ manager.php
@@ -57,25 +57,26 @@
 <?php
   //connect to database
   require_once("settings.php");
-  $sql_table = "cars";
+  $sql_table = "";
+  $attr = "order_id, first_name, last_name, order_time, order_status, order_product, order_quantity, order_cost"
 
   //verify connection
   if(!$conn) {
     die("Connection failed: " . mysqli_connect_error());
   } 
 
-  $query = "SELECT car_id, make, model, price FROM $sql_table";
+  $query = "SELECT $attr FROM $sql_table";
   $result = mysqli_query($conn, $query);
 
   //delete order on click
   if(isset($_GET["del_id"]) && isset($_GET["del_status"])) {
 
     $id = $_GET["del_id"];
-    $status = $_GET["del_status"];
+    $order_status = $_GET["del_status"];
 
-    if($status == "PENDING") {   
+    if($order_status == "PENDING") {   
 
-      $sql_del = "DELETE FROM cars WHERE car_id=$id";
+      $sql_del = "DELETE FROM $sql_table WHERE order_id=$id";
       $result = mysqli_query($conn,$sql_del); 
 
     
@@ -88,12 +89,12 @@
       }
   }
 
-  //update od status
+  //update od order_status
   if (isset($_POST["upd_id"])) {
-    $upd_status = $_POST["upd_status"];
+    $upd_order_status = $_POST["upd_order_status"];
     $upd_id = $_POST["upd_id"];
   
-    $sql_upd = "UPDATE cars SET make='$upd_status' WHERE car_id=$upd_id";
+    $sql_upd = "UPDATE $sql_table SET order_status='$upd_order_status' WHERE order_id=$upd_id";
     $result = mysqli_query($conn,$sql_upd);
       
     if($result) {
@@ -107,29 +108,29 @@
     header("location: payment.php");  
   }
   if(isset($_GET["all_order"])) {
-    $query = "SELECT car_id, make, model, price FROM $sql_table";
+    $query = "SELECT $attr FROM $sql_table";
     $result = mysqli_query($conn, $query);
   }
   if(isset($_GET["sort_name"])) {
     $search_query = $_GET["sort_name"];
-    $query = "SELECT * FROM $sql_table WHERE CONCAT(model, ' ', price) LIKE '%$search_query%'";
+    $query = "SELECT $attr FROM $sql_table WHERE CONCAT(model, ' ', price) LIKE '%$search_query%'";
     $result = mysqli_query($conn, $query);
   }
   if(isset($_GET["sort_prod"])) {
     $search_query = $_GET["sort_prod"];
-    $query = "SELECT * FROM $sql_table WHERE price LIKE '%$search_query%'";
+    $query = "SELECT $attr FROM $sql_table WHERE price LIKE '%$search_query%'";
     $result = mysqli_query($conn, $query);
   }
   if(isset($_GET["pending_prod"])) {
-    $query = "SELECT * FROM $sql_table WHERE make='PENDING'";
+    $query = "SELECT $attr FROM $sql_table WHERE order_status='PENDING'";
     $result = mysqli_query($conn, $query);
   }
   if(isset($_GET["cost_asc"])) {
-    $query = "SELECT * FROM $sql_table ORDER BY price ASC";
+    $query = "SELECT $attr FROM $sql_table ORDER BY price ASC";
     $result = mysqli_query($conn, $query);
   }
   if(isset($_GET["cost_desc"])) {
-    $query = "SELECT * FROM $sql_table ORDER BY price DESC";
+    $query = "SELECT $attr FROM $sql_table ORDER BY price DESC";
     $result = mysqli_query($conn, $query);
   }
 
@@ -146,14 +147,14 @@
       ."<th scope='col'>Product</th>\n"
       ."<th scope='col'>Quantity</th>\n"
       ."<th scope='col'>Total Cost</th>\n"
-      ."<th scope='col'>Status</th>\n"
+      ."<th scope='col'>order_status</th>\n"
       ."</tr>\n";
 
     //retrieve current record pointed by the result pointer
     while($row = mysqli_fetch_assoc($result)) {
     
       echo "<tr>\n";
-      echo "<td>",$row["car_id"],"</td>";
+      echo "<td>",$row["order_id"],"</td>";
       echo "<td>",$row["model"],"</td>";
       echo "<td>",$row["price"],"</td>";
       echo "<td>",$row["model"],"</td>";
@@ -163,18 +164,18 @@
 
       
       echo '<td><form method="post" action="manager.php">';
-      echo '<select name="upd_status">';
-      echo '<option value="PENDING"' . ($row["make"] == 'PENDING' ? ' selected="selected"' : '') . '>PENDING</option>';
-      echo '<option value="FULFILLED"' . ($row["status"] == 'FULFILLED' ? ' selected="selected"' : '') . '>FULFILLED</option>';
-      echo '<option value="PAID"' . ($row["status"] == 'PAID' ? ' selected="selected"' : '') . '>PAID</option>';
-      echo '<option value="ARCHIVED"' . ($row["status"] == 'ARCHIVED' ? ' selected="selected"' : '') . '>ARCHIVED</option>';
+      echo '<select name="upd_order_status">';
+      echo '<option value="PENDING"' . ($row["order_status"] == 'PENDING' ? ' selected="selected"' : '') . '>PENDING</option>';
+      echo '<option value="FULFILLED"' . ($row["order_status"] == 'FULFILLED' ? ' selected="selected"' : '') . '>FULFILLED</option>';
+      echo '<option value="PAID"' . ($row["order_status"] == 'PAID' ? ' selected="selected"' : '') . '>PAID</option>';
+      echo '<option value="ARCHIVED"' . ($row["order_status"] == 'ARCHIVED' ? ' selected="selected"' : '') . '>ARCHIVED</option>';
       echo '</select>';
-      echo '<input type="hidden" name="upd_id" value="' . $row["car_id"] . '"></td>';
+      echo '<input type="hidden" name="upd_id" value="' . $row["order_id"] . '"></td>';
       echo '<td><input type="submit" name="upd_od" value="update"></td>';
       echo '</form>';
       
       echo "<td>"; echo '<button class="del_btn">';
-      echo '<a class="del_link" href="manager.php?del_id='.$row["car_id"].'&del_status='.$row["make"].'">Delete</a>';
+      echo '<a class="del_link" href="manager.php?del_id='.$row["order_id"].'&del_status='.$row["make"].'">Delete</a>';
       echo '</button>'; echo "</td>";
     }
     echo "</table>\n"; 
