42ce0504931374939c769e1865e83f7a093814b6
LeQuangHai204 <lequanghai.private@gmail.com>
2023-04-06 22:32:10 +0700

    modified:   manager.php 	modified:   styles/manager.css

--- manager.php
+++ manager.php
@@ -11,232 +11,199 @@
 
 <?php include("includes/header.html"); ?>
 
-<?php  
+  <h1>Welcome, admin!</h1>
 
-  function sanitise_input($data) {
-      $data = trim($data);
-      $data = stripslashes($data);
-      $data = htmlspecialchars($data);
-      return $data;
+  <section>
+    <form class="form1" method="get" action="payment.php" novalidate="novalidate">
+      <input class="sm_btn" type="submit" value="add order">
+    </form>
+
+    <form class="form1" method="get" action="manager.php" novalidate="novalidate">
+      <input class="sm_btn" type="submit" value="view all orders">
+      <input type="hidden" name="all_order">
+    </form>
+
+    <form class="form1" method="get" action="manager.php" novalidate="novalidate">    
+      <input type="hidden" name="pending_prod">
+      <input class="sm_btn" type="submit" value="view pending order">
+    </form>
+
+    <form class="form1" method="get" action="manager.php" novalidate="novalidate">
+      <input class="sm_btn" type="submit" name="cost_asc" value="view cost ascending">
+      <input type="hidden" name="cost_asc">
+    </form>
+
+    <form class="form1" method="get" action="manager.php" novalidate="novalidate">
+      <input class="sm_btn" type="submit" name="cost_desc" value="view cost descending">
+      <input type="hidden" name="cost_desc">
+    </form>
+  </section>
+
+  <form class="form2" method="get" action="manager.php" novalidate="novalidate">    
+    <input class="inp_txt" type="text" name="sort_name">
+    <input class="sm_btn2" type="submit" value="search by customers name">
+  </form>
+
+  <form class="form2" method="get" action="manager.php" novalidate="novalidate">
+    <input class="inp_txt" type="text" name="sort_prod">
+    <input class="sm_btn2" type="submit" value="search by product name">
+  </form>';  
+
+<?php
+  //connect to database
+  require_once("settings.php");
+
+  $ord_table = "orders";
+  $ord_attr = "order_id, order_time, order_status, order_product, order_quantity, order_cost";
+  $cus_table = "customers";
+  $cus_attr = "first_name, last_name";
+  $show = "SELECT 
+    orders.order_id,
+    customers.first_name,
+    customers.last_name,
+    orders.order_time,
+    orders.order_product,
+    orders.order_quantity,
+    orders.order_cost,
+    orders.order_status
+    FROM orders
+    LEFT JOIN customers
+    ON orders.order_phone_number = customers.phone_number";
+
+  //verify connection
+  if(!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  } 
+
+  $result = mysqli_query($conn, $show);
+
+  function search($formName, $queryFilter) {
+    global $conn, $show, $result;
+    if(isset($_GET["$formName"])) {
+      $query = $show . " " . $queryFilter;
+      $result = mysqli_query($conn, $query);
+    }
   }
 
-  if(isset($_POST['user']) && isset($_POST['pass'])) {
-    $user = $_POST['user'];
-    $pass = $_POST['pass'];
-    sanitise_input($user);
-    sanitise_input($pass);
+  search("pendding_prod", "WHERE order_status='PENDING");
+  search("cost_acs", "ORDER BY order_cost ASC");
+  search("cost_desc", "ORDER BY order_cost DESC");
 
-    if($user == "admin" && $pass == "admin") {
-      echo '<h1>Welcome, admin!</h1>
+  //delete order on click
+  if(isset($_GET["del_id"]) && isset($_GET["del_status"])) {
 
-      <section>
-        <form class="form1" method="get" action="payment.php" novalidate="novalidate">
-          <input class="sm_btn" type="submit" value="add order">
-        </form>
+    $id = $_GET["del_id"];
+    $order_status = $_GET["del_status"];
 
-        <form class="form1" method="get" action="manager.php" novalidate="novalidate">
-          <input class="sm_btn" type="submit" value="view all orders">
-          <input type="hidden" name="all_order">
-        </form>
+    if($order_status == "PENDING") {   
 
-        <form class="form1" method="get" action="manager.php" novalidate="novalidate">    
-          <input type="hidden" name="pending_prod">
-          <input class="sm_btn" type="submit" value="view pending order">
-        </form>
+      $sql_del = "DELETE FROM $ord_table WHERE order_id=$id";
+      $result = mysqli_query($conn,$sql_del); 
 
-        <form class="form1" method="get" action="manager.php" novalidate="novalidate">
-          <input class="sm_btn" type="submit" name="cost_asc" value="view cost ascending">
-          <input type="hidden" name="cost_asc">
-        </form>
-
-        <form class="form1" method="get" action="manager.php" novalidate="novalidate">
-          <input class="sm_btn" type="submit" name="cost_desc" value="view cost descending">
-          <input type="hidden" name="cost_desc">
-        </form>
-      </section>
-
-      <form class="form2" method="get" action="manager.php" novalidate="novalidate">    
-        <input class="inp_txt" type="text" name="sort_name">
-        <input class="sm_btn2" type="submit" value="search by customers name">
-      </form>
-
-      <form class="form2" method="get" action="manager.php" novalidate="novalidate">
-        <input class="inp_txt" type="text" name="sort_prod">
-        <input class="sm_btn2" type="submit" value="search by product name">
-      </form>';  
-
-    //connect to database
-    require_once("settings.php");
-
-    $ord_table = "orders";
-    $ord_attr = "order_id, order_time, order_status, order_product, order_quantity, order_cost";
-    $cus_table = "customers";
-    $cus_attr = "first_name, last_name";
-    $show = "SELECT 
-      orders.order_id,
-      customers.first_name,
-      customers.last_name,
-      orders.order_time,
-      orders.order_product,
-      orders.order_quantity,
-      orders.order_cost,
-      orders.order_status
-      FROM orders
-      LEFT JOIN customers
-      ON orders.order_phone_number = customers.phone_number";
-
-    //verify connection
-    if(!$conn) {
-      die("Connection failed: " . mysqli_connect_error());
-    } 
-
-    $result = mysqli_query($conn, $show);
-
-    function search($formName, $queryFilter) {
-      global $conn, $show, $result;
-      if(isset($_GET["$formName"])) {
-        $query = $show . " " . $queryFilter;
-        $result = mysqli_query($conn, $query);
-      }
-    }
-
-    search("pendding_prod", "WHERE order_status='PENDING");
-    search("cost_acs", "ORDER BY order_cost ASC");
-    search("cost_desc", "ORDER BY order_cost DESC");
-
-    //delete order on click
-    if(isset($_GET["del_id"]) && isset($_GET["del_status"])) {
-
-      $id = $_GET["del_id"];
-      $order_status = $_GET["del_status"];
-
-      if($order_status == "PENDING") {   
-
-        $sql_del = "DELETE FROM $ord_table WHERE order_id=$id";
-        $result = mysqli_query($conn,$sql_del); 
-
-        if($result) {
-          header("location: manager.php");
-          echo '<p class="succ_mss">delete successfully</p>';
-        } 
-      } else {
-          echo '<p class="err_mss">only pending order can be deleted!</p>';
-        }
-    }
-
-    //update od order_status
-    if (isset($_POST["upd_id"])) {
-      $upd_order_status = $_POST["upd_order_status"];
-      $upd_id = $_POST["upd_id"];
-    
-      $sql_upd = "UPDATE $ord_table 
-        SET order_status = '$upd_order_status' 
-        WHERE order_id = $upd_id";
-      $result = mysqli_query($conn,$sql_upd);
-        
       if($result) {
         header("location: manager.php");
-        echo '<p class="succ_mss">update successfully</p>';
-      } else {
-        echo '<p class="err_mss">sth went wrong!</p>';
-      }
-    }
-    if(isset($_GET["all_order"])) {
-      $result = mysqli_query($conn, $show);
-    }
-    if(isset($_GET["sort_name"])) {
-      $search_query = $_GET["sort_name"];
-      
-      if(empty($sort_name)) {
-        echo "<p class='err_mss'>You must enter information to search</p>"; 
-      } else {
-        $query = "$show WHERE 
-          CONCAT($cus_table.first_name, ' ', $cus_table.last_name)
-          LIKE '%$search_query%'";
-        $result = mysqli_query($conn, $query);
-      }
-    }
-    if(isset($_GET["sort_prod"])) {
-      $search_query = $_GET["sort_prod"];
-    
-      if(empty($sort_prod)) {
-        echo "<p class='err_mss'>You must enter information to search</p>"; 
-      } else {
-        $query = "$show 
-          WHERE order.order_cost
-          LIKE '%$search_query%'";
-        $result = mysqli_query($conn, $query);
-      }  
-    }
-
-    if(!$result) {
-      echo "<p>Something went wrong with $query</p>";
+        echo '<p class="succ_mss">delete successfully</p>';
+      } 
     } else {
-      //display retrieve records    
-      echo '<table id="tb" border=1>';
-      echo "<tr>\n"
-        ."<th scope='col'>No.</th>\n"
-        ."<th scope='col'>First Name</th>\n"
-        ."<th scope='col'>Last Name</th>\n" 
-        ."<th scope='col'>Date</th>\n"
-        ."<th scope='col'>Product</th>\n"
-        ."<th scope='col'>Quantity</th>\n"
-        ."<th scope='col'>Total Cost</th>\n"
-        ."<th scope='col'>Status</th>\n"
-        ."</tr>\n";
-
-      //retrieve current record pointed by the result pointer
-      while($row = mysqli_fetch_assoc($result)) {
-      
-        echo "<tr>\n";
-        echo "<td>",$row["order_id"],"</td>";
-        echo "<td>",$row["first_name"],"</td>";
-        echo "<td>",$row["last_name"],"</td>";
-        echo "<td>",$row["order_time"],"</td>";
-        echo "<td>",$row["order_product"],"</td>";
-        echo "<td>",$row["order_quantity"],"</td>";
-        echo "<td>",$row["order_cost"],"</td>";
-
-
-        
-        echo '<td><form method="post" action="manager.php" novalidate="novalidate">';
-        echo '<select name="upd_order_status">';
-        echo '<option value="PENDING"' . ($row["order_status"] == 'PENDING' ? ' selected="selected"' : '') . '>PENDING</option>';
-        echo '<option value="FULFILLED"' . ($row["order_status"] == 'FULFILLED' ? ' selected="selected"' : '') . '>FULFILLED</option>';
-        echo '<option value="PAID"' . ($row["order_status"] == 'PAID' ? ' selected="selected"' : '') . '>PAID</option>';
-        echo '<option value="ARCHIVED"' . ($row["order_status"] == 'ARCHIVED' ? ' selected="selected"' : '') . '>ARCHIVED</option>';
-        echo '</select>';
-        echo '<input type="hidden" name="upd_id" value="' . $row["order_id"] . '"></td>';
-        echo '<td class="upd_btn"><input type="submit" name="upd_od" value="update"></td>';
-        echo '</form>';
-        
-        echo '<td class="upd_btn">'; echo '<button class="del_btn">';
-        echo '<a class="del_link" href="manager.php?del_id='.$row["order_id"].'&del_status='.$row["order_status"].'">Delete</a>';
-        echo '</button>'; echo "</td>";
+        echo '<p class="err_mss">only pending order can be deleted!</p>';
       }
-      echo "</table>\n"; 
-    }
-
-    //free up memory
-    mysqli_close($conn);
-
-    } else {
-      echo '
-        <form method="POST" action="manager.php" novalidate="novalidate">
-          User: <input type="text" name="user"></input><br/>
-          Pass: <input type="password" name="pass"></input><br/>
-          <input type="submit" name="submit" value="Log in"></input>
-        </form> ';
-      echo 'Incorrect password';
-    } 
-  } else {
-  echo '
-    <form id="login_form" method="POST" action="manager.php" novalidate="novalidate">
-      User: <input id="user" type="text" name="user"><br>
-      Pass: <input id="pass" type="password" name="pass"><br>
-      <input id="login_btn" type="submit" name="submit" value="Log in">
-    </form>';
   }
+
+  //update od order_status
+  if (isset($_POST["upd_id"])) {
+    $upd_order_status = $_POST["upd_order_status"];
+    $upd_id = $_POST["upd_id"];
+  
+    $sql_upd = "UPDATE $ord_table 
+      SET order_status = '$upd_order_status' 
+      WHERE order_id = $upd_id";
+    $result = mysqli_query($conn,$sql_upd);
+      
+    if($result) {
+      header("location: manager.php");
+      echo '<p class="succ_mss">update successfully</p>';
+    } else {
+      echo '<p class="err_mss">sth went wrong!</p>';
+    }
+  }
+  if(isset($_GET["all_order"])) {
+    $result = mysqli_query($conn, $show);
+  }
+  if(isset($_GET["sort_name"])) {
+    $search_query = $_GET["sort_name"];
+    
+    if(empty($sort_name)) {
+      echo "<p class='err_mss'>You must enter information to search</p>"; 
+    } else {
+      $query = "$show WHERE 
+        CONCAT($cus_table.first_name, ' ', $cus_table.last_name)
+        LIKE '%$search_query%'";
+      $result = mysqli_query($conn, $query);
+    }
+  }
+  if(isset($_GET["sort_prod"])) {
+    $search_query = $_GET["sort_prod"];
+  
+    if(empty($sort_prod)) {
+      echo "<p class='err_mss'>You must enter information to search</p>"; 
+    } else {
+      $query = "$show 
+        WHERE order.order_cost
+        LIKE '%$search_query%'";
+      $result = mysqli_query($conn, $query);
+    }  
+  }
+
+  if(!$result) {
+    echo "<p>Something went wrong with $query</p>";
+  } else {
+    //display retrieve records    
+    echo '<table id="tb" border=1>';
+    echo "<tr>\n"
+      ."<th scope='col'>No.</th>\n"
+      ."<th scope='col'>First Name</th>\n"
+      ."<th scope='col'>Last Name</th>\n" 
+      ."<th scope='col'>Date</th>\n"
+      ."<th scope='col'>Product</th>\n"
+      ."<th scope='col'>Quantity</th>\n"
+      ."<th scope='col'>Total Cost</th>\n"
+      ."<th scope='col'>Status</th>\n"
+      ."</tr>\n";
+
+    //retrieve current record pointed by the result pointer
+    while($row = mysqli_fetch_assoc($result)) {
+    
+      echo "<tr>\n";
+      echo "<td>",$row["order_id"],"</td>";
+      echo "<td>",$row["first_name"],"</td>";
+      echo "<td>",$row["last_name"],"</td>";
+      echo "<td>",$row["order_time"],"</td>";
+      echo "<td>",$row["order_product"],"</td>";
+      echo "<td>",$row["order_quantity"],"</td>";
+      echo "<td>",$row["order_cost"],"</td>";
+
+
+      
+      echo '<td><form method="post" action="manager.php" novalidate="novalidate">';
+      echo '<select name="upd_order_status">';
+      echo '<option value="PENDING"' . ($row["order_status"] == 'PENDING' ? ' selected="selected"' : '') . '>PENDING</option>';
+      echo '<option value="FULFILLED"' . ($row["order_status"] == 'FULFILLED' ? ' selected="selected"' : '') . '>FULFILLED</option>';
+      echo '<option value="PAID"' . ($row["order_status"] == 'PAID' ? ' selected="selected"' : '') . '>PAID</option>';
+      echo '<option value="ARCHIVED"' . ($row["order_status"] == 'ARCHIVED' ? ' selected="selected"' : '') . '>ARCHIVED</option>';
+      echo '</select>';
+      echo '<input type="hidden" name="upd_id" value="' . $row["order_id"] . '"></td>';
+      echo '<td class="upd_btn"><input type="submit" name="upd_od" value="update"></td>';
+      echo '</form>';
+      
+      echo '<td class="upd_btn">'; echo '<button class="del_btn">';
+      echo '<a class="del_link" href="manager.php?del_id='.$row["order_id"].'&del_status='.$row["order_status"].'">Delete</a>';
+      echo '</button>'; echo "</td>";
+    }
+    echo "</table>\n"; 
+  }
+
+  //free up memory
+  mysqli_close($conn);
 ?>
 
 <?php include("includes/footer.html"); ?>
